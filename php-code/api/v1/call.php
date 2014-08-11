42c28b76ac16e406677979f86ccac56eed0eef66
winklenurfer@gmail.com <alex.dickson@blackboard.com>
2014-08-11 16:56:38 -0700

    fixing from/to numbers

--- php-code/api/v1/call.php
+++ php-code/api/v1/call.php
@@ -3,15 +3,19 @@ require_once '../../twilio-php-master/Services/Twilio.php';
 header('Access-Control-Allow-Origin: *');
 
 // variables
-$body = $_GET[body];
+if ($_GET[body]) {
+	$body = $_GET[body];
+}
 
 // set your AccountSid and AuthToken from www.twilio.com/user/account
 $AccountSid = "AC24c73346a6248d4e5ccffb42333e0bbe";
 $AuthToken = "ae4c677b679e85e6f59a2556ea8470bc";
+$fromNumber = "415-969-9990";
+$toNumber = "415-595-2355";
  
 $client = new Services_Twilio($AccountSid, $AuthToken);
  
-$call = $client->account->calls->create("+14158675309", "+14155551212", "http://demo.twilio.com/docs/voice.xml", array());
+$call = $client->account->calls->create($fromNumber, $toNumber, "http://demo.twilio.com/docs/voice.xml", array());
  
 // Display a confirmation message on the screen
 echo "Sent call {$call->sid}";
--- php-code/api/v1/text.php
+++ php-code/api/v1/text.php
@@ -3,17 +3,21 @@ require_once '../../twilio-php-master/Services/Twilio.php';
 header('Access-Control-Allow-Origin: *');
 
 // variables
-$body = $_GET[body];
+if ($_GET[body]) {
+	$body = $_GET[body];
+}
 
 // set your AccountSid and AuthToken from www.twilio.com/user/account
 $AccountSid = "AC24c73346a6248d4e5ccffb42333e0bbe";
 $AuthToken = "ae4c677b679e85e6f59a2556ea8470bc";
+$fromNumber = "415-969-9990";
+$toNumber = "415-595-2355";
  
 $client = new Services_Twilio($AccountSid, $AuthToken);
  
 $message = $client->account->messages->create(array(
-    "From" => "415-969-9990",
-    "To" => "415-595-2355",
+    "From" => "$fromNumber",
+    "To" => "$toNumber",
     "Body" => "$body",
 ));
  
