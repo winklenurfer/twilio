96f5bcd7fd17dc6608bec9c0a945ab05e64aa7ec
winklenurfer@gmail.com <alex.dickson@blackboard.com>
2014-08-11 14:54:13 -0700

    adding php

---
+++ php/api/v1/call.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../../twilio-php-master/Services/Twilio.php';
+
+// set your AccountSid and AuthToken from www.twilio.com/user/account
+$AccountSid = "AC24c73346a6248d4e5ccffb42333e0bbe";
+$AuthToken = "ae4c677b679e85e6f59a2556ea8470bc";
+ 
+$client = new Services_Twilio($AccountSid, $AuthToken);
+ 
+$message = $client->account->messages->create(array(
+    "From" => "415-969-9990",
+    "To" => "415-595-2355",
+    "Body" => "Production message!",
+));
+ 
+// Display a confirmation message on the screen
+echo "Sent message {$message->sid}";
+
+?>
